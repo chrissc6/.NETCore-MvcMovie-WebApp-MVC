c3d3537095c2ac529bec32b52d75bb39f733436d
chris <christopherscaudill@gmail.com>
2020-03-21 04:34:04 -0400

    Add a model

---
+++ MvcMovie/MvcMovie/Data/MovieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        //DbSet<Movie> property the entity set "database table"
+        //An entity corresponds to a row in the table
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
